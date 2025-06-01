f5e88bedc15a4b4a61352a08acf16dc7c3188a3a
Sawaso <sawaso.lipovsky@gmail.com>
2025-06-01 16:01:11 +0200

    Added cmd argument reading

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "Expression.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) { std::cout << "Please enter an equation in infix notation like so: 10*2+3*((1+14)-18)-10"; return 0; }
 	try {
-		Expression e1("10*2+3*((1+14)-18)-10");
+		Expression e1(argv[1]);
+		std::cout << "Infix notation: ";
 		e1.print_infix();
+		std::cout << std::endl;
+		std::cout << "Postfix notation: ";
+		e1.print_postfix();
+		std::cout << std::endl;
+		std::cout << "Result: " << e1.evaluate();
 	}
 	catch (Exception e) { std::cout << e.message(); }
 }
